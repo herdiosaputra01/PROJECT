c73f4d12d5db83ad98dbb1a790d0ca9ec3d5821a
herdiosaputra01 <herdiosaputra06@gmail.com>
2024-11-13 14:50:12 +0700

    membuat file index

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project IS62</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<?php 
+    include_once ('navbar.php');
+?>
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+            <div class="card">
+    <div class="card-header text-center">
+        <h3>SISTEM INFORMASI MAHASISWA</h3>
+    </div>
+    <div class="card-body">
+    <h4>Selamat Datang, Haniffff</h4>
+    <p>Di Web Kita, kita-kita aja</p>
+    </div>
+    </div>
+        </div>
+
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
