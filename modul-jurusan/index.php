5d26e8d2eb6cd136d35cc1b600d79af0701e8220
herdiosaputra01 <herdiosaputra06@gmail.com>
2024-11-13 16:51:08 +0700

    penyesuaian tamplan index jurusan

--- modul-jurusan/index.php
+++ modul-jurusan/index.php
@@ -1,31 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Jurusan</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/all.css">
 </head>
+
 <body>
-<?php 
-    include_once ('../navbar.php');
-?>
+    <?php
+    include_once('../navbar.php');
+    ?>
 
-<div class="container">
-    <div class="row mt-5">
-        <div class="col-6 m-auto">
-            <div class="card">
-    <div class="card-header">
-        <h3>Data Jurusan</h3>
-    </div>
-    <div class="card-body">
-    <h4>Selamat Datang, Haniffff</h4>
-    <p>Di Web Kita, kita-kita aja</p>
-    </div>
-    </div>
-        </div>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-8 m-auto">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="float-start">Data Jurusan</h3>
+                        <span class="float-end"><a class="btn btn-primary btn-sm" href=""><i class="fa-solid fa-square-plus"></i> tambah data</a></span>
+                    </div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Kode</th>
+                                    <th scope="col">Nama Jurusan</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>
+                                        <a class="btn btn-primary btn-sm" href=""><i class="fa-solid fa-pen-to-square"></i></a>
+                                        <a class="btn btn-danger btn-sm" href=""><i class="fa-solid fa-eraser"></i></a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
 
-    <script src="../js/bootstrap.js"></script>
-    <script src="../js/bootstrap.bundle.js"></script>
+            <script src="../js/bootstrap.js"></script>
+            <script src="../js/bootstrap.bundle.js"></script>
+            <script src="../js/all.js"></script>
 </body>
+
 </html>
