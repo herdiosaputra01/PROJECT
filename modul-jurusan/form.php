cc84d77a7e0171e300f8455d887e92b0205609f2
herdiosaputra01 <herdiosaputra06@gmail.com>
2024-12-04 16:31:52 +0700

    proses edit (modul jurusan selesai)

--- modul-jurusan/form.php
+++ modul-jurusan/form.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Event Manager</title>
+    <title>Data Jurusan</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../css/all.css">
 </head>
